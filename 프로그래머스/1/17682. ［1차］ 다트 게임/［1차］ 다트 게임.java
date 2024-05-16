290de72867c0864b16eb94ef168165d4acbb3ca4
서진만SeoJinmann <115869486+sjmmics@users.noreply.github.com>
2024-05-16 16:20:43 +0900

    Update ［1차］ 다트 게임.java

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,41 +1,55 @@
+import java.util.ArrayList;
+
 class Solution {
     public int solution(String dartResult) {
-        
-        return getScore(dartResult);
+        ArrayList<String> list = transList(dartResult);
+        return getScore(list);
     }
-    public int getScore(String darts) {
-        int totScore = 0;
+    public ArrayList<String> transList(String str) {
+        ArrayList<String> answer = new ArrayList<>();
+        boolean isTwoDigit = false;
+        for (int i = 0; i < str.length(); i++) {
+            char letter = str.charAt(i);
+            if (Character.isDigit(letter)) {
+                if (isTwoDigit) {
+                    isTwoDigit = false;
+                    continue;
+                }
+                if (Character.isDigit(str.charAt(i + 1))) {
+                    answer.add("10");
+                    isTwoDigit = true;
+                } else {
+                    answer.add(Character.toString(letter));
+                }
+            } else {
+                answer.add(Character.toString(letter));
+            }
+        }
+        return answer;
+    }
+    public int getScore(ArrayList<String> list) {
+        int result = 0;
         int prevScore = 0;
         int crrScore = 0;
-        boolean isTwoDigit = false;
-        for (int i = 0; i < darts.length(); i++) {
-            char crrLetter = darts.charAt(i);
-            
-            if (Character.isDigit(crrLetter)) {
-                if (!isTwoDigit) {
-                    totScore += prevScore;
-                    prevScore = crrScore;
-                    if (Character.isDigit(darts.charAt(i + 1))) {
-                        crrScore = 10;
-                        isTwoDigit = true;
-                    } else {
-                        crrScore = Character.getNumericValue(crrLetter);
-                    }
-                } else {
-                    isTwoDigit = false;
-                }
-            } else if (crrLetter == 'D') {
-                crrScore = (int) Math.pow(crrScore, 2);    
-            } else if (crrLetter == 'T') {
+        for (String crr : list) {
+            if (crr.equals("D")) {
+                crrScore = (int) Math.pow(crrScore, 2);
+            } else if (crr.equals("T")) {
                 crrScore = (int) Math.pow(crrScore, 3);
-            } else if (crrLetter == '*') {
-                prevScore *= 2;
+            } else if (crr.equals("*")) {
                 crrScore *= 2;
-            } else if (crrLetter == '#') {
+                prevScore *= 2;
+            } else if (crr.equals("#")) {
                 crrScore = -crrScore;
+            } else if (crr.equals("S")) {
+                continue;
+            } else {
+                result += prevScore;
+                prevScore = crrScore;
+                crrScore = Integer.parseInt(crr);
             }
-            
         }
-        return totScore + crrScore + prevScore;
+        return result + crrScore + prevScore;
     }
+    
 }
