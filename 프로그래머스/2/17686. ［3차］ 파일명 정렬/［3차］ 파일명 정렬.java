0c3dfa6f761e0a318c48dca0f228897132a7558e
서진만SeoJinmann <115869486+sjmmics@users.noreply.github.com>
2024-06-08 21:52:51 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.27 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,47 +1,59 @@
 import java.util.Arrays;
-import java.util.Objects;
 
 class Solution {
     public String[] solution(String[] files) {
-        String[] answer = getSorted(files);
-        return answer;
-    }
-    public String[] getSorted(String[] files) {
-        Object[][] arr = new Object[files.length][3];
-        for (int i = 0; i < files.length; i++) {
-            String str = files[i];
-            int leftNumIdx = 0;
-            for (int j = 0; j < str.length(); j++) {
-                if (Character.isDigit(str.charAt(j))) {
-                    leftNumIdx = j;
-                    break;
+        MyFile[] arr = new MyFile[files.length];
+        for (int i = 0 ; i < files.length; i++) {
+            arr[i] = new MyFile(files[i], i);
+        }
+        Arrays.sort(arr, ((o1, o2) -> {
+            if (o1.head.equalsIgnoreCase(o2.head)) {
+                if (o1.number == o2.number) {
+                    return o1.originalIndex - o2.originalIndex;
+                } else {
+                    return o1.number - o2.number;
                 }
+            } else {
+                return o1.head.compareToIgnoreCase(o2.head);
             }
-            int rightNumIdx = getRightIdx(str, leftNumIdx);
-            arr[i][0] = str.substring(0, leftNumIdx);
-            arr[i][1] = Integer.parseInt(str.substring(leftNumIdx, rightNumIdx + 1));
-            arr[i][2] = (int) i;
-        }
-        Arrays.sort(arr, (o1, o2) ->
-        {
-            if (((String) o1[0]).equalsIgnoreCase((String) o2[0])) {
-                if (o1[1] == o2[1]) return (int) o1[2] - (int) o2[2];
-                else return (int) o1[1] - (int) o2[1];}
-            else {return ((String) o1[0]).compareToIgnoreCase((String) o2[0]);}
-        });
-        String[] answer = new String[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            answer[i] = files[(int) arr[i][2]];
+        }));
+        String[] answer = new String[files.length];
+        for (int i = 0; i < files.length; i++) {
+            answer[i] = files[arr[i].originalIndex];
         }
         return answer;
     }
-    public int getRightIdx(String str, int leftNumIdx) {
-        
-        for (int i = leftNumIdx + 1; i < str.length(); i++) {
+}
+
+class MyFile {
+    String head;
+    int number;
+    int originalIndex;
+    
+    public MyFile(String str, int index) {
+        int leftNumberIdx = getLeftNumberIdx(str);
+        int rightNumberIdx = getRightNumberIdx(str, leftNumberIdx);
+        this.head = str.substring(0, leftNumberIdx);
+        this.number = Integer.parseInt(str.substring(leftNumberIdx, rightNumberIdx + 1));
+        this.originalIndex = index;
+    }
+
+    private int getRightNumberIdx(String str, int leftNumberIdx) {
+        for (int i = leftNumberIdx + 1; i < str.length(); i++) {
             if (!Character.isDigit(str.charAt(i))) {
                 return i - 1;
             }
         }
         return str.length() - 1;
     }
+
+    private int getLeftNumberIdx(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                return i;
+            }
+        }
+        return str.length() - 1;
+    }
+    
 }
