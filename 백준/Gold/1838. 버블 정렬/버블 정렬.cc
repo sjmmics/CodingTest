5c75fd2aef2a8589a38caddc271d7690b7da8761
서진만SeoJinmann <115869486+sjmmics@users.noreply.github.com>
2024-07-21 16:03:04 +0900

    [Gold I] Title: 버블 정렬, Time: 100 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Gold/1838. 버블 정렬/버블 정렬.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, res;
+vector<pair<int, int>> vt;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N;
+    vt.resize(N);
+    for (int i = 0; i < N; i++) {
+        int x;  cin >> x;
+        vt[i].first = x;
+        vt[i].second = i;
+    }
+    sort(vt.begin(), vt.end());
+    for (int i = 0; i < N; i++) {
+        if (res < vt[i].second - i) {
+            res = vt[i].second - i;
+        }
+    }
+    cout << res << "\n";
+}
